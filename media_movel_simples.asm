75d70a3a6423ff8a6727e929db3d64f718f2efef
Alex Davis Neuwiem da Silva <108128476+AlexDavisNeuwiem@users.noreply.github.com>
2022-11-26 12:08:56 -0300

    Add files via upload

--- media_movel_simples.asm
+++ media_movel_simples.asm
@@ -7,12 +7,24 @@
 # $s3 = Armazena N
 # $s4 = Ponteiro para Saida01
 # $s5 = Ponteiro para Saida02
+# $s6 = Armazena N da Saida01
+# $s7 = Armazena N da Saida02
 
 .data
-	N:		.asciiz "Digite N: "
+	N:		.asciiz "\nDigite N: "
 	
-	Valor:		.asciiz "Digite um valor (0 para finalizar): "
-
+	Valor:		.asciiz "\nDigite um valor (0 para finalizar): "
+	
+	Espaco:		.asciiz "  "
+	
+	MensFinal:	.asciiz "\nValor   M.M.Curta   M.M.Longa   Tendencia\n"
+	
+	Constante:	.asciiz	"Constante\n"
+	
+	Queda:		.asciiz	"Queda\n"
+	
+	Alta:		.asciiz	"Alta\n"
+	
 	.align 2
 	Entradas:	.float
 	
@@ -36,12 +48,18 @@ Main:
 	# $s4 = ponteiro para Saidas01
 	move	$s4, $s1
 	
+	# $s6 armazena N
+	move	$s6, $s3
+	
 	# $s1 = ponteiro para Saidas02
 	la	$s1, Saida02
 	jal	MediaMovel
 	
 	# $s5 = ponteiro para Saidas02
 	move	$s5, $s1
+	
+	# $s7 armazena N
+	move	$s7, $s3
 	
 	# Procedimento pra comparar os valores das medias moveis
 	jal	Compara
@@ -51,18 +69,6 @@ Main:
 
 # Procedimento para armazenar na memoria os valores digitados ----------------------#
 Inicio:
-	# Mostrando mensagem na tela
-    	la      $a0, N
-    	li      $v0, 4
-    	syscall
-
-	# 5 = comando para ler inteiro do teclado
-	li 	$v0, 5
-	syscall
-
-	# $s3 armazena N
- 	move 	$s3, $v0
- 	
  	# $s0 = ponteiro para Entradas
  	la	$s0, Entradas
  	
@@ -114,6 +120,18 @@ LoopEntrada:
 
 # Procedimento pra calcular a media movel simples ----------------------------------#
 MediaMovel:
+	# Mostrando mensagem na tela
+    	la      $a0, N
+    	li      $v0, 4
+    	syscall
+
+	# 5 = comando para ler inteiro do teclado
+	li 	$v0, 5
+	syscall
+
+	# $s3 armazena N
+ 	move 	$s3, $v0
+
 	# $t0 = ponteiro auxiliar para Entradas
 	move	$t0, $s0
 	
@@ -177,7 +195,136 @@ CalculaMedia:
 
 # Procedimento pra comparar os valores das medias moveis ---------------------------#
 Compara:
+	# Mostrando a mensagem final na tela
+    	la      $a0, MensFinal
+    	li      $v0, 4
+    	syscall
+
+	# $t3 = ponteiro pra Entradas
+	move	$t3, $s0
 	
+	# Reseta $t2
+	li	$t2, 0
+
+	# Verifica qual a maior media movel
+	blt	$s7, $s6, SegundoMenor
+
+	# $t0 e $t1 tambem sao ponteiros
+	move	$t0, $s4
+	move	$t1, $s5
+	
+	j	LoopCompara
+
+SegundoMenor:
+	# $t0 e $t1 tambem sao ponteiros
+	move	$t0, $s5
+	move	$t1, $s4
+
+	j	LoopCompara
+
+LoopCompara:
+	# $f0 armazena valores da menor media movel
+	l.s	$f0, 0($t0)
+	
+	# $f1 armazena valores da maior media movel
+	l.s	$f1, 0($t1)
+	
+	# $f2 armazena valores de Entradas
+	l.s	$f2, 0($t3)
+	
+	# Incrementando o contador
+	addi	$t2, $t2, 1
+	
+
+	# Mostrando o valor de Entradas na tela
+    	mov.s	$f12, $f2
+    	li      $v0, 2
+    	syscall
+ 	
+	# Mostrando espaco na tela
+    	la      $a0, Espaco
+    	li      $v0, 4
+    	syscall
+    	
+    	# Mostrando o valor da menor media movel na tela
+    	mov.s	$f12, $f0
+    	li      $v0, 2
+    	syscall
+ 	
+	# Mostrando espaco na tela
+    	la      $a0, Espaco
+    	li      $v0, 4
+    	syscall
+ 	
+	# Mostrando o valor da menor media movel na tela
+    	mov.s	$f12, $f1
+    	li      $v0, 2
+    	syscall
+ 	
+	# Mostrando espaco na tela
+    	la      $a0, Espaco
+    	li      $v0, 4
+    	syscall
+
+
+	# Verifica se $f0 = $f1
+	c.eq.s	$f0, $f1
+	
+	# Se $f0 = $f1, entao vai para Igual
+	bc1t  	Igual
+
+	# Verifica se $f0 < $f1
+	c.lt.s 	$f0, $f1
+	
+	# Se $f0 < $f1, entao vai para Menor
+	bc1t	Menor
+	
+	# Mostrando alta na tela
+    	la      $a0, Alta
+    	li      $v0, 4
+    	syscall
+    	
+    	# Atualizando os valores
+    	addi	$t0, $t0, 4
+    	addi	$t1, $t1, 4
+    	addi	$t3, $t3, 4
+	
+	# Se $t2 = $s2, deve-se parar de comparar valores
+	bne	$t2, $s2, LoopCompara
+
+	# Retornando para Main
+ 	jr	$ra
+ 	
+Igual:
+	# Mostrando constante na tela
+    	la      $a0, Constante
+    	li      $v0, 4
+    	syscall
+    	
+    	# Atualizando os valores
+    	addi	$t0, $t0, 4
+    	addi	$t1, $t1, 4
+    	addi	$t3, $t3, 4
+
+	# Se $t2 = $s2, deve-se parar de comparar valores
+	bne	$t2, $s2, LoopCompara
+
+	# Retornando para Main
+ 	jr	$ra
+
+Menor:
+	# Mostrando queda na tela
+    	la      $a0, Queda
+    	li      $v0, 4
+    	syscall
+    	
+    	# Atualizando os valores
+    	addi	$t0, $t0, 4
+    	addi	$t1, $t1, 4
+    	addi	$t3, $t3, 4
+
+	# Se $t2 = $s2, deve-se parar de comparar valores
+	bne	$t2, $s2, LoopCompara
 
 	# Retornando para Main
  	jr	$ra
